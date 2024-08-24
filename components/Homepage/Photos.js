5c7f78f2bc92292f44ed29a2ceba0b1a6a8e0c47
Dominik <dominik.jojczyk@gmail.com>
2024-08-24 10:04:02 +0200

    1.1

--- components/Homepage/Photos.js
+++ components/Homepage/Photos.js
@@ -4,61 +4,128 @@ import { useRef } from "react";
 import { FaInstagram, FaFacebook, FaTiktok } from "react-icons/fa";
 import { useInView, motion } from "framer-motion";
 import Link from "next/link";
+import SliderComponent from "../UI/Slider/Slider";
+
+const images = [
+  {
+    src: "/photos-slider/1.jpg",
+    alt: "zdjecie ze srodka Huki Muki",
+  },
+  {
+    src: "/photos-slider/2.jpg",
+    alt: "zdjecie ze srodka Huki Muki",
+  },
+  {
+    src: "/photos-slider/3.jpg",
+    alt: "zdjecie ze srodka Huki Muki",
+  },
+  {
+    src: "/photos-slider/4.jpg",
+    alt: "zdjecie ze srodka Huki Muki",
+  },
+  {
+    src: "/photos-slider/5.jpg",
+    alt: "zdjecie ze srodka Huki Muki",
+  },
+  {
+    src: "/photos-slider/6.jpg",
+    alt: "zdjecie ze srodka Huki Muki",
+  },
+  {
+    src: "/photos-slider/7.jpg",
+    alt: "zdjecie ze srodka Huki Muki",
+  },
+  {
+    src: "/photos-slider/8.jpg",
+    alt: "zdjecie ze srodka Huki Muki",
+  },
+  {
+    src: "/photos-slider/9.jpg",
+    alt: "zdjecie ze srodka Huki Muki",
+  },
+  {
+    src: "/photos-slider/10.jpg",
+    alt: "zdjecie ze srodka Huki Muki",
+  },
+  {
+    src: "/photos-slider/11.jpg",
+    alt: "zdjecie ze srodka Huki Muki",
+  },
+  {
+    src: "/photos-slider/12.jpg",
+    alt: "zdjecie ze srodka Huki Muki",
+  },
+  {
+    src: "/photos-slider/13.jpg",
+    alt: "zdjecie ze srodka Huki Muki",
+  },
+
+  {
+    src: "/photos-slider/15.jpg",
+    alt: "zdjecie ze srodka Huki Muki",
+  },
+  {
+    src: "/photos-slider/17.jpg",
+    alt: "zdjecie ze srodka Huki Muki",
+  },
+];
+
 export default function Photos() {
   const skillRef = useRef();
   const isSkillRefinView = useInView(skillRef);
   return (
-    <motion.div
-      initial={{ opacity: 0, scale: 0.75 }}
-      animate={
-        isSkillRefinView
-          ? { opacity: 1, scale: 1 }
-          : { opacity: 0, scale: 0.85 }
-      }
-      transition={{ delay: 0.3, duration: 1, ease: "easeOut" }}
-    >
-      <div className={classes.container} ref={skillRef}>
-        <div className={classes.textContainer}>
-          <h4>Galeria Klubu</h4>
-          <p>
-            Poczuj klimat imprez w Huki Muki Pub Club! To tylko przedsmak –
-            więcej zdjęć znajdziesz na naszym Instagramie. Sprawdź, jak bawimy
-            się co weekend, i dołącz do nas, aby przeżyć niezapomniane chwile.
-            Nasze wydarzenia to połączenie najlepszej muzyki, wyjątkowej
-            atmosfery i wspaniałego towarzystwa. Nie przegap żadnej okazji, aby
-            być częścią tego, co czyni Huki Muki miejscem, o którym wszyscy
-            mówią. Zobacz, jak nasi goście tworzą wspomnienia, które zostają na
-            zawsze!
-          </p>
+    <>
+      <motion.div
+        initial={{ opacity: 0, scale: 0.75 }}
+        animate={
+          isSkillRefinView
+            ? { opacity: 1, scale: 1 }
+            : { opacity: 0, scale: 0.85 }
+        }
+        transition={{ delay: 0.3, duration: 1, ease: "easeOut" }}
+      >
+        <div className={classes.container} ref={skillRef}>
+          <div className={classes.textContainer}>
+            <h4>Galeria Klubu</h4>
+            <p>
+              Poczuj klimat imprez w Huki Muki Pub Club! To tylko przedsmak –
+              więcej zdjęć znajdziesz na naszym Instagramie. Sprawdź, jak bawimy
+              się co weekend, i dołącz do nas, aby przeżyć niezapomniane chwile.
+              Nasze wydarzenia to połączenie najlepszej muzyki, wyjątkowej
+              atmosfery i wspaniałego towarzystwa. Nie przegap żadnej okazji,
+              aby być częścią tego, co czyni Huki Muki miejscem, o którym
+              wszyscy mówią. Zobacz, jak nasi goście tworzą wspomnienia, które
+              zostają na zawsze!
+            </p>
 
-          <div
-            className={classes.socialsIcons}
-            whileHover={{ scale: 1.15 }}
-            whileTap={{
-              scale: 0.8,
-            }}
-          >
-            <Link
-              href={"https://www.facebook.com/hukimukikrakow"}
-              className={classes.facebook}
+            <div
+              className={classes.socialsIcons}
+              whileHover={{ scale: 1.15 }}
+              whileTap={{
+                scale: 0.8,
+              }}
             >
-              <FaFacebook />
-            </Link>
-            <Link
-              href={"https://www.tiktok.com/@hukimukikrakow"}
-              className={classes.tiktok}
-            >
-              <FaTiktok />
-            </Link>
-            <Link
-              className={classes.instagram}
-              href={"https://www.instagram.com/hukimuki/"}
-            >
-              <FaInstagram />
-            </Link>
+              <Link
+                href={"https://www.facebook.com/hukimukikrakow"}
+                className={classes.facebook}
+              >
+                <FaFacebook />
+              </Link>
+              <Link
+                href={"https://www.tiktok.com/@hukimukikrakow"}
+                className={classes.tiktok}
+              >
+                <FaTiktok />
+              </Link>
+              <Link
+                className={classes.instagram}
+                href={"https://www.instagram.com/hukimuki/"}
+              >
+                <FaInstagram />
+              </Link>
+            </div>
           </div>
-        </div>
-        <div className={classes.imagesContainer}>
+          {/* <div className={classes.imagesContainer}>
           <Image
             src={"/photos/1.jpg"}
             width={500}
@@ -88,8 +155,12 @@ export default function Photos() {
             layout="responsive"
             alt="zdjecie z klubu&pubu hukimuki"
           ></Image>
+        </div> */}
+          <div className={classes.sliderContainer}>
+            <SliderComponent images={images} />
+          </div>
         </div>
-      </div>
-    </motion.div>
+      </motion.div>
+    </>
   );
 }
---
+++ components/UI/Slider/Slider.js
@@ -0,0 +1,75 @@
+"use client";
+import classes from "./Slider.module.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { IoIosArrowForward } from "react-icons/io";
+import Image from "next/image";
+
+export default function SliderComponent({ images }) {
+  const carouselSettings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    cssEase: "linear",
+    initialSlide: 0,
+    nextArrow: (
+      <div>
+        <div className={classes.rightArrow}>
+          <IoIosArrowForward />
+        </div>
+      </div>
+    ),
+    prevArrow: (
+      <div>
+        <div className={classes.rotate}>
+          <IoIosArrowForward />
+        </div>
+      </div>
+    ),
+    responsive: [
+      {
+        breakpoint: 1224,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 680,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <div className={classes.container}>
+      <Slider {...carouselSettings} className={classes.slider}>
+        {images.map((image, index) => (
+          <Image
+            key={index}
+            src={image.src}
+            height={image.height || 550}
+            width={image.width || 450}
+            layout="responsive"
+            alt={image.alt || "Slider Image"}
+          />
+        ))}
+      </Slider>
+    </div>
+  );
+}
